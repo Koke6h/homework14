b9d384f50369efb551b7b3b0d2552914db7ab5a4
User <git@github.com:Koke6h/helloworld.git>
2024-04-29 17:56:59 +0400

    end homework

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Transport car = new Car ("car1", 4);
+        Transport car2 = new Car("car2", 4);
+
+
+        Transport truck = new Truck("track1", 8);
+        Transport truck2 = new Truck("track2",8);
+
+
+        Transport bicycle = new Bicycle("Bicycle1",2);
+        Transport bicycle2 = new Bicycle("Bicycle2",2);
+
+
+        ServiceStation station = new ServiceStation();
+        station.check(car);
+        station.check(car2);
+        station.check(bicycle);
+        station.check(bicycle2);
+        station.check(truck);
+        station.check(truck2);
     }
 }
